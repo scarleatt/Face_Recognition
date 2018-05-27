c35323909f34a2b425d7bbbce8f4ff1e5951b4f5
resccu <fawnliu2333@gmail.com>
2018-05-27 13:27:44 +0800

    add readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-![dot_res](https://github.com/scarleatt/Face_Recognition/blob/master/image/dot_res.jpg)
+1. First, look at a picture and find all the faces in it![rect_res](https://github.com/scarleatt/Face_Recognition/blob/master/image/res_rect.png)
 
-![rect_res](https://github.com/scarleatt/Face_Recognition/blob/master/image/res_rect.png)
+2. Second, be able to pick out unique features of the face that you can use to tell it apart from other people— like how big the eyes are, how long the face is, etc.![dot_res](https://github.com/scarleatt/Face_Recognition/blob/master/image/dot_res.jpg)
+3. Finally, compare the unique features of that face to all the people you already know to determine the person’s name.
+
+ 
--- step-1_find-faces.py
+++ step-1_find-faces.py
@@ -1,36 +1,61 @@
+import matplotlib.pyplot as plt
 import sys
 import dlib
+import numpy as np
 from skimage import io
+import openface
+import cv2
+import os
 
-# Take the image file name from the command line
-file_name = '1.jpg'
+predictor_model = "shape_predictor_68_face_landmarks.dat"
 
-# Create a HOG face detector using the built-in dlib class
 face_detector = dlib.get_frontal_face_detector()
+face_aligner = openface.AlignDlib(predictor_model)
 
-win = dlib.image_window()
+if sys.argv[1].split('.')[1]=='jpg':
+    file_name = sys.argv[1]
+    image = io.imread(file_name)
+    detected_faces = face_detector(image, 0)
+    print("I found {} faces in the file {}".format(len(detected_faces), file_name))
 
-# Load the image into an array
-image = io.imread(file_name)
+    for i, face_rect in enumerate(detected_faces):
+        alignedFace = face_aligner.align(534, image, face_rect,
+                                         landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
+        name = "./img_rect_cuted/{}_img_rect_cuted_{}.jpg".format(file_name.split('.')[0], i)
+        cv2.imwrite(name, alignedFace)
 
-# Run the HOG face detector on the image data.
-# The result will be the bounding boxes of the faces in our image.
-detected_faces = face_detector(image, 1)
+        I = io.imread(name, as_grey=True)
+        io.imsave(name, I)
+        print(name)
+else:
+    imgDir=sys.argv[1]
+    if sys.argv[2]:
+        dst=sys.argv[2]
+    else:
+        dst='./'
+    for file in os.listdir(imgDir):
+        if (file.split('.')[1]=='jpg'):
+            file_name=imgDir+file
+            image = io.imread(file_name)
+            detected_faces = face_detector(image, 0)
+            print("I found {} faces in the file {}".format(len(detected_faces), file_name))
 
-print("I found {} faces in the file {}".format(len(detected_faces), file_name))
+            img=plt.imread(file_name)
+            fig, ax = plt.subplots()
+            ax.imshow(img, cmap='gray')
+            for i, face_rect in enumerate(detected_faces):
+                l=face_rect.left()
+                r=face_rect.right()
+                t=face_rect.top()
+                b=face_rect.bottom()
+                plt.vlines(l, b, t, csolors="b", linewidth=2)
+                plt.hlines(t, l, r, colors="b", linewidth=2)
+                plt.hlines(b, l, r, colors="b", linewidth=2)
+                plt.vlines(r, b, t, colors="b", linewidth=2)
+                print("- Face #{} found at Left: {} Top: {} Right: {} Bottom: {}".format(i, l, t, r, b))
 
-# Open a window on the desktop showing the image
-win.set_image(image)
-
-# Loop through each face we found in the image
-for i, face_rect in enumerate(detected_faces):
-
-	# Detected faces are returned as an object with the coordinates
-	# of the top, left, right and bottom edges
-	print("- Face #{} found at Left: {} Top: {} Right: {} Bottom: {}".format(i, face_rect.left(), face_rect.top(), face_rect.right(), face_rect.bottom()))
-
-	# Draw a box around each face we found
-	win.add_overlay(face_rect)
-
-# Wait until the user hits <enter> to close the window
-dlib.hit_enter_to_continue()
+            plt.axis('off')
+            
+            fig.savefig('{}img_{}_rect_all.jpg'.format(dst,file.split('.')[0]),
+                        dpi=200,bbox_inches='tight')
+            plt.close(fig)
